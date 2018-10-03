85475114a0193aab0b474b736b267645124e46d9
Martin Helmut Dominguez Alvarez <martinhelmutmh@gmail.com>
2018-10-03 14:33:31 -0500

    CSS working

--- nss/nss/settings.py
+++ nss/nss/settings.py
@@ -14,8 +14,11 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(BASE_DIR,'templates') #Creo una variable con el path apuntando a mi carpeta templates
-
+TEMPLATE_DIR = os.path.join(BASE_DIR,"templates") #Creo una variable con el path apuntando a mi carpeta templates
+STATIC_DIR=os.path.join(BASE_DIR,"static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+print(__file__)
+print(TEMPLATE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -81,7 +84,7 @@ DATABASES = {                                   #Nos conectamos a la base de dat
         'NAME': 'Kunigo',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': '127.0.0.1',
+        'HOST': '192.168.64.2',
         'PORT': '3306',
     }
 }
@@ -125,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+                  STATIC_DIR,
+                  ]
