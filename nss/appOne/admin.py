683091005f7fce6fad29a0bcd86a820192fc08a4
AngieGE <30639215+AngieGE@users.noreply.github.com>
2018-10-03 15:54:02 -0500

    problems in table proyecto

--- nss/appOne/admin.py
+++ nss/appOne/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 #Importamos todas las clases (tablas) de las apps.
-from .models import proyecto, area
+from .models import proyecto, area, location
 
 #Anotamos las clases (tablas) para poder verlas en la pagina de admin.
 admin.site.register(proyecto)
 admin.site.register(area)
+admin.site.register(location)
