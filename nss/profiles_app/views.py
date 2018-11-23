fa3002e0ccc58ac16e7c9d67fd8ba16204fecb3d
linetes <A01207499@itesm.mx>
2018-11-22 21:19:57 -0600

    search user

--- nss/profiles_app/views.py
+++ nss/profiles_app/views.py
@@ -2,10 +2,31 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from account_app.models import Profile
+from django.db.models import Q
+from functools import reduce
+from django.views import generic
+from braces.views import SelectRelatedMixin
+import operator
 # Create your views here.
-class ProfileList(ListView):
+class ProfileList(SelectRelatedMixin, ListView):
     model = Profile
     template_name = 'profiles_app/profile_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self):
+        result = super(ListView, self).get_queryset()
+
+        if self.request.GET.get('q'):
+            query = self.request.GET.get('q')
+            query_list = query.split()
+            result = result.filter(
+                reduce(operator.and_,
+                       (Q(user__username__icontains=q) for q in query_list))
+            )
+        return result
 
 class ProfileDetail(DetailView):
     model = Profile
